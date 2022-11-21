aa8e20c4979d8f73cfd90af3d7e3ba0a3e14b8ad
Alex91021 <alex9102146@gmail.com>
2022-11-21 19:06:11 +0300

    Commit 3

--- NewsPortal/NewsPortal/settings.py
+++ NewsPortal/NewsPortal/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'NewsPortal.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'templates/flatpages')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
